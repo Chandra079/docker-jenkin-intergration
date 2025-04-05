cadd92c993c817f074834ebbfca7c94ddb0e8c8b
Chandra079 <95139049+Chandra079@users.noreply.github.com>
2025-04-05 22:43:22 +0530

    Added GetMapping

--- src/main/java/com/chandra/dockerjenkinintergration/DockerjenkinintergrationApplication.java
+++ src/main/java/com/chandra/dockerjenkinintergration/DockerjenkinintergrationApplication.java
@@ -2,12 +2,14 @@ package com.chandra.dockerjenkinintergration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DockerjenkinintergrationApplication {
 
+    @GetMapping("/")
     public String getMessage(){
         System.out.println("DockerjenkinintergrationApplication");
         return "DockerjenkinintergrationApplication";
